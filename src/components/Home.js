b203008728046ae6b4ce7ba04bb457626d042cab
Aryan Shrivastav <aryanshr91003@gmail.com>
2023-02-08 15:10:35 +0530

    initialized tailwind

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import bgImg from './img/sky.jpg'
+import galaxy from './img/galaxy.jpg'
+function Home() {
+  return (
+    <div className="h-[100vh]">
+        Hi
+    </div>
+  )
+}
+
+export default Home
