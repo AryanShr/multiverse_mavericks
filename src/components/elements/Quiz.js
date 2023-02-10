ac98a34e82668d4ecd65511bee31d213f3bd7810
Aryan Shrivastav <aryanshr91003@gmail.com>
2023-02-10 10:41:32 +0530

    changed icons

--- src/components/elements/Quiz.js
+++ src/components/elements/Quiz.js
@@ -1,11 +1,10 @@
 import React,{useState} from 'react'
 import quiz from './img/quiz.png'
-import glowquiz from './img/quizglow.png'
 function Quiz() {
     const[glow, setglow] = useState(false);
     return (
       <div className='h-32 w-64 hover:scale-150 transition-all' onMouseEnter={()=>setglow(true)} onMouseLeave={()=>setglow(false)}>
-          {!glow? <img src={quiz} alt="book" className=''/>: <img src={glowquiz} alt="book" className=''/>}
+          {<img src={quiz} alt="book" className=''/>}
           <p className='text-center text-2xl relative'>Quiz</p>
       </div>
     )
